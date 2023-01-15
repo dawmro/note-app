062d73b1040e397df5c81e018268644798103765
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-15 02:44:23 +0100

    create buttons, labels and entry fields

--- note-app.py
+++ note-app.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 import sqlite3
+from datetime import datetime
+
+
 
 class NoteApp:
     def __init__(self):
@@ -15,6 +18,23 @@ class NoteApp:
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS notes 
                             (topic TEXT, note TEXT, timestamp TEXT)''')
         self.conn.commit()
+        
+        # create buttons
+        self.topic_label = tk.Label(self.root, text="Topic:")
+        self.topic_entry = tk.Entry(self.root)
+        self.note_label = tk.Label(self.root, text="Note:")
+        self.note_entry = tk.Entry(self.root)
+        self.add_button = tk.Button(self.root, text="Add Note", command='')
+        
+        # add GUI elements to the root window
+        self.topic_label.grid(row=0, column=0, sticky='e')
+        self.topic_entry.grid(row=0, column=1)
+        self.note_label.grid(row=1, column=0, sticky='e')
+        self.note_entry.grid(row=1, column=1)
+        self.add_button.grid(row=2, column=1)
+
+        
+
         
 
 
