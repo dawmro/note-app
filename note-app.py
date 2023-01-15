798916ce66f9378899edafc8ab37dc59925f783e
dawid mrotek <DawidMrotek@protonmail.ch>
2023-01-15 02:35:05 +0100

    create main loop

--- note-app.py
+++ note-app.py
@@ -6,3 +6,12 @@ class NoteApp:
         self.root = tk.Tk()
         self.root.title("Note App")
         self.root.geometry('620x300')
+        
+
+
+
+if __name__ == "__main__":
+    # create app instance
+    app = NoteApp()
+    # run app
+    app.root.mainloop()
